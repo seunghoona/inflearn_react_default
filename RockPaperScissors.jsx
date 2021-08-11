d89a31c4c926227b45778f546b008a4bb9877354
naseunghoo <naseunghoo@gmail.com>
2021-08-11 18:18:17 +0900

    init

---
+++ RockPaperScissors.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class RockPaperScissors extends Component {
+    render() {
+        return (
+            <div>
+                <h1>
+                    하이
+                </h1>
+            </div>
+        )
+    }
+}
---
+++ client.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import RockPaperScissors from './RockPaperScissors';
+
+
+ReactDOM.render(<RockPaperScissors/>, document.querySelector("#root"));
