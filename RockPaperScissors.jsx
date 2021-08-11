5c3a31ad627323d1297ee74a39b516b578b6b73b
naseunghoo <naseunghoo@gmail.com>
2021-08-11 19:38:35 +0900

    complete 5

--- RockPaperScissors.jsx
+++ RockPaperScissors.jsx
@@ -1,13 +1,118 @@
 import React, { Component } from 'react'
+/**
+constructor → render → ref → componentDidMount → (화면이보임) 
+(setState/props 바뀔 때 리렌더링이 있어남) → shouldComponentUpdate →render → componentDidUpdate
+부모가 나를 없앴을 때 →**componentWillUnmount → 소멸
+ */
 
-export default class RockPaperScissors extends Component {
+const rspCoords = {
+    바위: '0',
+    가위: '-142px',
+    보: '-284px',
+}
+
+const scores = {
+    가위: 1,
+    바위: rspCoords.바위,
+    보: -1
+}
+
+const computerChoice = (imgCoord) => {
+    return Object.entries(rspCoords).find(function(v) {
+      return v[1] === imgCoord;
+    })[0];
+  };
+
+class RockPaperScissors extends Component {
+    //constructor 영역
+    state = {
+        result: '',
+        imgCoord: rspCoords.바위,
+        score: 0,
+    }
+
+    interval;
+
+    componentDidMount() { // 컴포넌트가 첫 렌더링 후 -> //비동기요청을 처리 
+        this.interval = setInterval(this.changeHadle, 100);
+    }
+
+    changeHadle = () => {
+        const { imgCoord } = this.state;
+            if (imgCoord === rspCoords.바위) {
+                this.setState({
+                    imgCoord: rspCoords.가위
+                })
+            } else if (imgCoord === rspCoords.가위) {
+                this.setState({
+                    imgCoord: rspCoords.보
+                });
+            } else if (imgCoord === rspCoords.보) {
+                this.setState({
+                    imgCoord: rspCoords.바위
+                });
+            }
+    }
+
+
+    componentWillUnmount() { //리렌더링 후 
+        clearInterval(this.interval);
+    }
+
+    componentDidUpdate() {  //부모는 자식 컴포넌트를 없애는 경우 실행됨 //비동기요청 정리 
+
+    }
+
+
+    onClickBtn = (choice) => {
+        const {imgCoord} = this.state;
+        clearInterval(this.interval);
+        const myScore = scores[choice];
+        const cpuScore = scores[computerChoice(imgCoord)];
+        const diff = myScore - cpuScore;
+        if (diff === 0) {
+            this.setState({
+                result: '비겼습니다.'
+            })
+
+        } else if ([-1, 2].includes(diff)) {
+            this.setState((prevState) => {
+                return {
+                    result: "이겼습니다.",
+                    score: prevState.score + 1,
+                }
+            });
+        } else {
+            this.setState((prevState) => {
+                return {
+                    result: "졌습니다.",
+                    score: prevState.score - 1,
+                }
+            });
+        }
+        setTimeout(()=>{
+            this.interval=setInterval(this.changeHadle, 100);
+        })
+    };
+
     render() {
+        const { result, score, imgCoord } = this.state;
+
         return (
-            <div>
-                <h1>
-                    하이
-                </h1>
-            </div>
+            <>
+                <div id="computer" style={{ background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${imgCoord} 0` }} />
+
+                <div>
+                    <button id="rock" className="btn"    onClick={()=> this.onClickBtn('바위')}>바위</button>
+                    <button id="scissor" className="btn" onClick={()=> this.onClickBtn('가위')}>가위</button>
+                    <button id="paper" className="btn"   onClick={()=> this.onClickBtn('보')}>보</button>
+                </div>
+                <div>{result}</div>
+                <div>현재 {score}점</div>
+            </>
         )
     }
 }
+
+
+export default RockPaperScissors;
